105163a7e43f348b27a01226cfd89c22aca6388d
Alpastx <alpeshbhagwatkar45@gmail.com>
2025-04-25 23:03:05 +0530

    added web/wherearetherobots.py

--- web/wherearetherobots.py
+++ web/wherearetherobots.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup as bs
 
-url = "https://jupiter.challenges.picoctf.org/problem/56830/"
+url = ""
 robots_url = url+"1bb4c.html"
 response = requests.get(robots_url)
 soup = bs(response.text, 'html.parser')
